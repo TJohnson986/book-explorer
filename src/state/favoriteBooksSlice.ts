d40972007e05e00a80d4a47fef1af39d2d896374
Tyler Johnson <tyler@csdconsultants.us>
2025-10-31 16:47:18 -0400

    cleans up reducer logic

--- src/state/favoriteBooksSlice.ts
+++ src/state/favoriteBooksSlice.ts
@@ -3,27 +3,23 @@ import { createSlice } from '@reduxjs/toolkit';
 export const favoriteBooksSlice = createSlice({
   name: 'favoriteBooks',
   initialState: {
-    value: 0,
+    favoriteBooks: [] as string[],
   },
   reducers: {
-    increment: state => {
-      // Redux Toolkit allows us to write "mutating" logic in reducers. It
-      // doesn't actually mutate the state because it uses the Immer library,
-      // which detects changes to a "draft state" and produces a brand new
-      // immutable state based off those changes
-      state.value += 1;
+    increment: (state, id) => {
+      if (!state.favoriteBooks.includes(id.payload)) {
+        state.favoriteBooks.push(id.payload);
+        return;
+      }
     },
-    decrement: state => {
-      state.value -= 1;
-    },
-    incrementByAmount: (state, action) => {
-      state.value += action.payload;
+    decrement: (state, id) => {
+      state.favoriteBooks = state.favoriteBooks.filter(
+        bookId => bookId !== id.payload,
+      );
     },
   },
 });
 
-// Action creators are generated for each case reducer function
-export const { increment, decrement, incrementByAmount } =
-  favoriteBooksSlice.actions;
+export const { increment, decrement } = favoriteBooksSlice.actions;
 
 export default favoriteBooksSlice.reducer;
