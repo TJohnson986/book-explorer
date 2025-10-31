794eaeac0c3a6ade050eb2af36bc3d87be29ae77
Tyler Johnson <tyler@csdconsultants.us>
2025-10-31 16:48:40 -0400

    adds error handling via modal and other null handling

--- src/navigators/screens/HomeScreen.tsx
+++ src/navigators/screens/HomeScreen.tsx
@@ -1,94 +1,88 @@
 import { useNavigation } from '@react-navigation/native';
+import { NativeStackNavigationProp } from '@react-navigation/native-stack';
+import { FlatList, Pressable, Text } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { useGetBooksQuery } from '../../services/getBooks';
 import {
-  Button,
-  Image,
-  ScrollView,
-  StyleSheet,
-  Text,
-  View,
-} from 'react-native';
-import React, { useState } from 'react';
-import { Counter } from '../../components/Counter';
-import { useGetPostsQuery } from '../../services/getBooks';
-import { Card, Searchbar } from 'react-native-paper';
+  ActivityIndicator,
+  Card,
+  Modal,
+  PaperProvider,
+  Portal,
+  Searchbar,
+} from 'react-native-paper';
+import { BookResult } from '../../components/BookResult/BookResult';
+import { BookResultProps } from '../../types/BookResult';
+import { RootStackParamList } from '../../types/NavigationTypes';
 
 export const HomeScreen = () => {
-  const navigation = useNavigation();
-  const { data, error } = useGetPostsQuery();
+  const [modalVisible, setModalVisible] = React.useState(false);
   const [searchQuery, setSearchQuery] = useState('');
 
+  const showModal = () => setModalVisible(true);
+  const hideModal = () => setModalVisible(false);
+  const containerStyle = { backgroundColor: 'white', margin: 20 };
+
+  const navigation =
+    useNavigation<NativeStackNavigationProp<RootStackParamList>>();
+
+  const { data, error, isLoading } = useGetBooksQuery();
+
+  useEffect(() => {
+    if (error) {
+      showModal();
+    }
+  }, [error]);
+
   const searchResults = data?.items.filter(
-    (item: any) =>
+    (item: BookResultProps) =>
       searchQuery === '' ||
-      item.volumeInfo.title.toLowerCase().includes(searchQuery.toLowerCase()),
+      item.volumeInfo.title.toLowerCase().includes(searchQuery.toLowerCase()) ||
+      item.volumeInfo.authors
+        ?.join(' ')
+        .toLowerCase()
+        .includes(searchQuery.toLowerCase()),
+  );
+
+  const renderResultCard = (item: BookResultProps) => (
+    <Pressable
+      onPress={() => navigation.navigate('Details', { item })}
+      style={({ pressed }) => [
+        {
+          backgroundColor: pressed ? 'lightgray' : 'white',
+        },
+      ]}
+    >
+      <Card key={item.id} style={{ margin: 10 }}>
+        <BookResult {...item} />
+      </Card>
+    </Pressable>
   );
 
   return (
-    <ScrollView>
-      <Button
-        title="Go to Details"
-        onPress={() => navigation.navigate('Details' as never)}
-      />
-      <Searchbar
-        placeholder="Search By Title"
-        onChangeText={setSearchQuery}
-        value={searchQuery}
-      />
-      <Text>{searchQuery}</Text>
-      <View style={styles.container}>
-        <Image
-          style={styles.logo}
-          onError={e => {
-            console.log('Image load error', e);
-          }}
-          source={{
-            uri: 'http://books.google.com/books/content?id=it0RBAAAQBAJ&printsec=frontcover&img=1&zoom=5&edge=curl&source=gbs_api',
-          }}
-        />
-      </View>
-
-      {searchResults?.map((item: any) => {
-        {
-          console.log(
-            'item.volumeInfo.imageLinks?.smallThumbnail',
-            item.volumeInfo.imageLinks?.smallThumbnail,
-          );
-        }
-        return (
-          <Card key={item.id} style={{ margin: 10 }}>
-            <Card.Title
-              title={item.volumeInfo.title}
-              // left={() => (
-              //   <Image source={{ uri: item.volumeInfo.imageLinks.thumbnail }} />
-              // )}
-            />
-            <Card.Cover
-              source={{
-                uri: 'http://books.google.com/books/content?id=it0RBAAAQBAJ&printsec=frontcover&img=1&zoom=5&edge=curl&source=gbs_api',
-              }}
-            />
-            <Card.Content>
-              <Text>Authors: {item.volumeInfo.authors?.join(', ')}</Text>
-              <Text>Published Date: {item.volumeInfo.publishedDate}</Text>
-            </Card.Content>
-          </Card>
-        );
-      })}
-      {/* <Counter /> */}
-    </ScrollView>
+    <>
+      <PaperProvider>
+        <Portal>
+          <Searchbar
+            placeholder="Search By Title"
+            onChangeText={setSearchQuery}
+            value={searchQuery}
+          />
+          {isLoading && <ActivityIndicator animating={true} size="large" />}
+          <FlatList
+            data={searchResults}
+            renderItem={({ item }) => renderResultCard(item)}
+            keyExtractor={item => item.id}
+          />
+          <Modal
+            visible={modalVisible}
+            onDismiss={hideModal}
+            contentContainerStyle={containerStyle}
+          >
+            <Text style={{ margin: 20 }}>Oh No! Something went wrong.</Text>
+          </Modal>
+        </Portal>
+      </PaperProvider>
+    </>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  tinyLogo: {
-    width: 50,
-    height: 50,
-  },
-  logo: {
-    width: 100,
-    height: 100,
-  },
-});
