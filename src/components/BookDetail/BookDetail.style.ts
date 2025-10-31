77007e3c554a44c793ce21fac62ce60e9b6f11d1
Tyler Johnson <tyler@csdconsultants.us>
2025-10-31 16:53:19 -0400

    creates separate stylesheets

---
+++ src/components/BookDetail/BookDetail.style.ts
@@ -0,0 +1,28 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  bodyTextContainer: {
+    marginLeft: 10,
+    marginTop: 20,
+  },
+  cardActions: {
+    flexDirection: 'row',
+    alignItems: 'flex-end',
+    justifyContent: 'space-between',
+    marginBottom: 10,
+    marginTop: 40,
+  },
+  container: {
+    flexDirection: 'column',
+  },
+  tinyLogo: {
+    width: 50,
+    height: 50,
+  },
+  thumbnail: {
+    width: 200,
+    height: 300,
+    paddingVertical: 10,
+    alignSelf: 'center',
+  },
+});
---
+++ src/components/BookResult/BookResult.style.ts
@@ -0,0 +1,17 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  bodyTextContainer: {
+    marginLeft: 10,
+    flex: 1,
+  },
+  container: {
+    flex: 1,
+    flexDirection: 'row',
+  },
+  thumbnail: {
+    width: 100,
+    height: 150,
+    paddingVertical: 10,
+  },
+});
