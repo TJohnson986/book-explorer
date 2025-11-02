505a41dc0cbdb1a4c92d90f8f8f0a2209284be1f
Tyler Johnson <tyler@csdconsultants.us>
2025-11-01 20:10:53 -0400

    code cleanup

--- App.tsx
+++ App.tsx
@@ -5,9 +5,9 @@
  * @format
  */
 
-import { StatusBar, StyleSheet, useColorScheme } from 'react-native';
+import { StatusBar, useColorScheme } from 'react-native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
-import { MainNavigator } from './src/navigators/MainNavigator';
+import MainNavigator from './src/navigators/MainNavigator';
 import { Provider } from 'react-redux';
 import { store } from './src/state/store';
 
@@ -22,16 +22,10 @@ function App() {
     <SafeAreaProvider>
       <Provider store={store}>
         <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-        <MainNavigator />
+        <MainNavigator isDarkMode={isDarkMode} />
       </Provider>
     </SafeAreaProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-});
 
 export default App;
